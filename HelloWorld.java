221a79921dd5c3dd76b6002b4b4ac018f1744772
vjdeshmukhgit <vijay@intelifx.com>
2017-05-23 14:59:50 -0400

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,6 @@
 public class HelloWorld
 {
      public static void main(String[] args)   {
-           System.out.printIn("Hello, 
-World!");
+           System.out.printIn("Hello World!");
      }
 }
